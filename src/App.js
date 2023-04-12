766fb988dbe729b415041103cc9281dd55b97d02
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 09:24:06 +0300

    new component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
+import "./App.css"
+import Likes from "./Likes"
 
 function App() {
   return (
-    <div className="App">
-      <div className="wrap">
-        <div className="card">
-          <div className="card-image">
-            <img src="./sea.jpg" alt="surfing"/>
+    <div className='App'>
+      <div className='wrap'>
+        <div className='card'>
+          <div className='card-image'>
+            <img src='./sea.jpg' alt='surfing' />
+            <Likes likes='3' />
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
