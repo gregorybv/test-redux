a421cf506b2afafd77c5ffc015bd4012e6053db9
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 13:27:49 +0300

    new edits

--- src/Comments.js
+++ src/Comments.js
@@ -1,14 +1,39 @@
 import SingleComment from "./SingleComment"
+import { commentCreate } from "./redux/actions"
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import uniqid from "uniqid"
 
 function Comments(props) {
-  // console.log("comments props >", props)
+  const [textComment, setTextComment] = useState("")
+  const comments = useSelector((state) => {
+    const { commentsReducer } = state
+    return commentsReducer.comments
+  })
+
+  const dispatch = useDispatch()
+
+  const handleInput = (e) => {
+    setTextComment(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const id = uniqid()
+    dispatch(commentCreate(textComment, id))
+  }
+
   return (
     <div className='card-comments'>
-      <form className='comments-item-create'>
-        <input type='text' />
+      <form onSubmit={handleSubmit} className='comments-item-create'>
+        <input type='text' value={textComment} onChange={handleInput} />
         <input type='submit' hidden />
       </form>
-      <SingleComment />
+      {comments.length &&
+        comments.map((res) => {
+          return <SingleComment key={res.id} data={res} />
+        })}
     </div>
   )
 }
