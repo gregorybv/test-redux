242aa3c1e4a5013302fc4e97ca60ca761f988b62
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 16:56:51 +0300

    new edits

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css"
+import { useSelector } from "react-redux"
 import Comments from "./Comments"
 import Likes from "./Likes"
 import Title from "./Title"
 import Spin from "./Spin"
 
 function App() {
+  const error = useSelector((state) => state.appReducer.error)
+
   return (
     <div className='App'>
       <div className='wrap'>
         <Spin />
         <div className='card'>
+          {error && <div className='error-message'>{error}</div>}
           <div className='card-image'>
             <img src='./sea.jpg' alt='surfing' />
             <Title />
