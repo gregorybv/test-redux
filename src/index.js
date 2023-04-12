771fe09976acbcf10bb6259b0c5039e7d3de9516
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 12:08:13 +0300

    new edits

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import { createStore } from "redux"
+import { rootReducer } from "./redux/rootReducer"
+import { Provider } from "react-redux"
+import "./index.css"
+import App from "./App"
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
