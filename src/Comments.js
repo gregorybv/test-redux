08f8824aa87bee667919b815fa3c1a8281c26698
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 13:01:45 +0300

    new component

---
+++ src/Comments.js
@@ -0,0 +1,16 @@
+import SingleComment from "./SingleComment"
+
+function Comments(props) {
+  // console.log("comments props >", props)
+  return (
+    <div className='card-comments'>
+      <form className='comments-item-create'>
+        <input type='text' />
+        <input type='submit' hidden />
+      </form>
+      <SingleComment />
+    </div>
+  )
+}
+
+export default Comments
