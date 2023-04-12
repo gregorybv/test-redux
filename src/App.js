b23d2f95a01f5ed6fcf4654d556ec7b9d67cdf6d
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 13:02:00 +0300

    new connect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css"
+import Comments from "./Comments"
 import Likes from "./Likes"
 import Title from "./Title"
 
@@ -12,6 +13,7 @@ function App() {
             <Title />
             <Likes />
           </div>
+          <Comments />
         </div>
       </div>
     </div>
