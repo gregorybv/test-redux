95b6eec64f7d761ae61f420e857cba2f980803bc
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 10:37:57 +0300

    new edits

--- src/Likes.js
+++ src/Likes.js
@@ -1,11 +1,20 @@
+import { connect } from "react-redux"
+
 function Likes(props) {
-  console.log(props)
+  // console.log(props)
   return (
     <div className='button-controls'>
-      <button>❤ {props.like}</button>
+      <button>❤ {props.likes}</button>
       <button>Dislike</button>
     </div>
   )
 }
 
-export default Likes
+function mapStateToProps(state) {
+  // console.log("mapStateToProps >", state)
+  return {
+    likes: state.likes,
+  }
+}
+
+export default connect(mapStateToProps)(Likes)
