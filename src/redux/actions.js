4156d0f1bb2968e0be9cdce930ded57e204bf5b7
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 15:50:29 +0300

    new actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,6 +4,8 @@ import {
   INPUT_TEXT,
   COMMENT_CREATE,
   COMMENT_UPDATE,
+  COMMENT_DELETE,
+  COMMENTS_LOAD,
 } from "./types"
 
 export function incrementLikes() {
@@ -36,5 +38,25 @@ export function commentUpdate(text, id) {
   return {
     type: COMMENT_UPDATE,
     data: { text, id },
+  }
+}
+
+export function commentDelete(id) {
+  return {
+    type: COMMENT_DELETE,
+    id,
+  }
+}
+
+export function commentsLoad(id) {
+  return async (dispatch) => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/comments?_limit=10"
+    )
+    const jsonData = await response.json()
+    dispatch({
+      type: COMMENTS_LOAD,
+      data: jsonData,
+    })
   }
 }
