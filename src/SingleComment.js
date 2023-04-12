5d060b1f9402c25857edb3f1201030ef9384c860
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 15:15:53 +0300

    new edits

--- src/SingleComment.js
+++ src/SingleComment.js
@@ -1,8 +1,16 @@
 import { useState, useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { commentUpdate } from "./redux/actions"
 
 function SingleComment({ data }) {
   const [commentText, setCommentText] = useState("")
   const { text, id } = data
+  const dispatch = useDispatch()
+
+  const handleUpdate = (e) => {
+    e.preventDefault()
+    dispatch(commentUpdate(commentText, id))
+  }
 
   useEffect(() => {
     if (text) {
@@ -15,7 +23,7 @@ function SingleComment({ data }) {
   }
 
   return (
-    <form className='comments-item'>
+    <form onSubmit={handleUpdate} className='comments-item'>
       <div className='comments-item-delete'>&times;</div>
       <input type='text' value={commentText} onChange={handleInput} />
       <input type='submit' hidden />
