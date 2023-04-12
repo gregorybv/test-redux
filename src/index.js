e1e6fad2c0190ce924a1a7017bcc8f619154aa12
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 16:56:55 +0300

    new edits

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import thunk from "redux-thunk"
 import { createStore, compose, applyMiddleware } from "redux"
 import { rootReducer } from "./redux/rootReducer"
 import { Provider } from "react-redux"
+import { spamFilter } from "./redux/middleware"
 import "./index.css"
 import App from "./App"
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, spamFilter)
     // строка ниже выдает ошибку приложения
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
