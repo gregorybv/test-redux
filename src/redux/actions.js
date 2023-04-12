cfdb85d8ba6380218cb6d53fad30b18b06e1b193
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-12 16:57:23 +0300

    new actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -8,6 +8,8 @@ import {
   COMMENTS_LOAD,
   LOADER_DISPLAY_ON,
   LOADER_DISPLAY_OFF,
+  ERROR_DISPLAY_ON,
+  ERROR_DISPLAY_OFF,
 } from "./types"
 
 export function incrementLikes() {
@@ -62,20 +64,43 @@ export function loaderOff() {
   }
 }
 
+export function errorOn(text) {
+  return (dispatch) => {
+    dispatch({
+      type: ERROR_DISPLAY_ON,
+      text,
+    })
+    setTimeout(() => {
+      dispatch(errorOff())
+    }, 2000)
+  }
+}
+
+export function errorOff() {
+  return {
+    type: ERROR_DISPLAY_OFF,
+  }
+}
+
 export function commentsLoad(id) {
   return async (dispatch) => {
-    dispatch(loaderOn())
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/comments?_limit=10"
-    )
-    const jsonData = await response.json()
+    try {
+      dispatch(loaderOn())
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/comments?_limit=10"
+      )
+      const jsonData = await response.json()
 
-    setTimeout(() => {
-      dispatch({
-        type: COMMENTS_LOAD,
-        data: jsonData,
-      })
+      setTimeout(() => {
+        dispatch({
+          type: COMMENTS_LOAD,
+          data: jsonData,
+        })
+        dispatch(loaderOff())
+      }, 1000)
+    } catch (err) {
+      dispatch(errorOn("Ошибка API"))
       dispatch(loaderOff())
-    }, 1000)
+    }
   }
 }
